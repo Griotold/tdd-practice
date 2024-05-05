d7ab60a0d288afb37d46aeeab72030a6568bda36
Griotold <gotjd9773@naver.com>
2024-05-05 10:23:26 +0900

    [암호검사기 - 책버젼] 리팩토링 -> 검증 대상인 PasswordStrengthMeter 필드로 추출, 검증 로직 메서드로 추출

--- src/test/java/org/study/tddpasswordchecker/passwordchecker2/PasswordStrengthMeterTest.java
+++ src/test/java/org/study/tddpasswordchecker/passwordchecker2/PasswordStrengthMeterTest.java
@@ -12,34 +12,35 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class PasswordStrengthMeterTest {
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertThat(result).isEqualTo(expStr);
+    }
+
     // 첫 번째 테스트는 가장 쉽거나 가장 예외적인 상황을 선택해야 한다.
     @DisplayName("암호가 모든 조건을 충족하면 암호 강도는 '강함'이어야 함")
     @Test
     void meetsAllCriteria_Then_String() throws Exception {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@AB");
-        assertThat(result).isEqualTo(PasswordStrength.STRONG);
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
 
-        PasswordStrength result2 = meter.meter("abc1!Add");
-        assertThat(result2).isEqualTo(PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
     @DisplayName("길이만 8글자 미만이고 나머지 조건은 충족하는 경우 -> '보통'")
     @Test
     void meetsOtherCriteria_except_for_Length_Then_Normal() throws Exception {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab12!@A");
-        assertThat(result).isEqualTo(PasswordStrength.NORMAL);
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
 
-        PasswordStrength result2 = meter.meter("Ab12!c");
-        assertThat(result2).isEqualTo(PasswordStrength.NORMAL);
+        assertStrength("Ab12!c", PasswordStrength.NORMAL);
     }
 
     @DisplayName("숫자를 포함하지 않고 나머지 조건은 충족하는 경우 -> '보통'")
     @Test
     void meetsOtherCriteria_except_for_number_Then_Normal() throws Exception {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab!@ABqwer");
-        assertThat(result).isEqualTo(PasswordStrength.NORMAL);
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
     }
+
+
 }
