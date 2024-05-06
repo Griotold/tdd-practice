6cf4fcd208ed87cb3765c085f1de4627c169a0fe
Griotold <gotjd9773@naver.com>
2024-05-06 10:32:23 +0900

    [영화 평점 구하기] 1. Movie 생성 직후 영화 평점은 0

---
+++ src/test/java/org/study/tddpasswordchecker/movie/Movie.java
@@ -0,0 +1,8 @@
+package org.study.tddpasswordchecker.movie;
+
+public class Movie {
+
+    public int average() {
+        return 0;
+    }
+}
---
+++ src/test/java/org/study/tddpasswordchecker/movie/MovieTest.java
@@ -0,0 +1,26 @@
+package org.study.tddpasswordchecker.movie;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 켄트 벡 - 테스트 주도 개발 - 예제 1. 영화 average 구하기
+ * 백명석 유튜브 참고
+ * https://www.youtube.com/watch?v=wmHV6L0e1sU&t=1552s
+ * */
+public class MovieTest {
+
+    @Test
+    void canCreateMovie() throws Exception {
+        Movie movie = new Movie();
+    }
+
+    @DisplayName("Movie 생성 직 후 average 는 0 ")
+    @Test
+    void should_return_o_when_just_created() throws Exception {
+        Movie movie = new Movie();
+        assertThat(movie.average()).isEqualTo(0);
+    }
+}
