e103df712a61b5bd3de71e246c58129fd16a0687
Griotold <gotjd9773@naver.com>
2024-05-06 10:38:22 +0900

    [영화 평점 구하기] 2. 점수 1을 주고 평점을 구하면 1

--- src/test/java/org/study/tddpasswordchecker/movie/Movie.java
+++ src/test/java/org/study/tddpasswordchecker/movie/Movie.java
@@ -2,7 +2,15 @@ package org.study.tddpasswordchecker.movie;
 
 public class Movie {
 
+    private int sumOfPoint;
+    private int countOfRate;
+
     public int average() {
-        return 0;
+        return countOfRate == 0 ? 0 : sumOfPoint / countOfRate;
+    }
+
+    public void rate(int point) {
+        this.sumOfPoint += point;
+        countOfRate++;
     }
 }
--- src/test/java/org/study/tddpasswordchecker/movie/MovieTest.java
+++ src/test/java/org/study/tddpasswordchecker/movie/MovieTest.java
@@ -9,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * 켄트 벡 - 테스트 주도 개발 - 예제 1. 영화 average 구하기
  * 백명석 유튜브 참고
  * https://www.youtube.com/watch?v=wmHV6L0e1sU&t=1552s
- * */
+ */
 public class MovieTest {
 
     @Test
@@ -22,5 +22,14 @@ public class MovieTest {
     void should_return_o_when_just_created() throws Exception {
         Movie movie = new Movie();
         assertThat(movie.average()).isEqualTo(0);
+    }
+
+    @DisplayName("점수 1을 주고 평점을 구하면 1")
+    @Test
+    void should_return_1_when_was_rated() throws Exception {
+        Movie movie = new Movie();
+        movie.rate(1);
+        assertThat(movie.average()).isEqualTo(1);
+
     }
 }
