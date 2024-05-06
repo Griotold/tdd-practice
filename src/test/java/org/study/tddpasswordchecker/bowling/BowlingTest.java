2b54846e4f7cae3935130bfc2a72980b02caf3ea
Griotold <gotjd9773@naver.com>
2024-05-06 11:16:14 +0900

    [볼링 게임] Game 클래스 생성

---
+++ src/test/java/org/study/tddpasswordchecker/bowling/BowlingTest.java
@@ -0,0 +1,18 @@
+package org.study.tddpasswordchecker.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.Socket;
+
+/**
+ * 볼링 게임
+ * 백명석 유튜브 참고
+ * https://www.youtube.com/watch?v=fFwDMzML7hI
+ * */
+public class BowlingTest {
+
+    @Test
+    void canCreateGame() throws Exception {
+        Game game = new Game();
+    }
+}
---
+++ src/test/java/org/study/tddpasswordchecker/bowling/Game.java
@@ -0,0 +1,4 @@
+package org.study.tddpasswordchecker.bowling;
+
+public class Game {
+}
