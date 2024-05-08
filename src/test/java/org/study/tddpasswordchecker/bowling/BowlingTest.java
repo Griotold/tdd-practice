3244193225a032c53e6b20003d67db0ba3b75e51
Griotold <gotjd9773@naver.com>
2024-05-08 15:57:46 +0900

    [볼링 게임] 2. gutterGame + Game 객체 필드로 추출

--- src/test/java/org/study/tddpasswordchecker/bowling/BowlingTest.java
+++ src/test/java/org/study/tddpasswordchecker/bowling/BowlingTest.java
@@ -1,9 +1,13 @@
 package org.study.tddpasswordchecker.bowling;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.net.Socket;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * 볼링 게임
@@ -12,14 +16,25 @@ import java.net.Socket;
  * */
 public class BowlingTest {
 
-    @Test
-    void canCreateGame() throws Exception {
-        Game game = new Game();
+    private Game game;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        game = new Game();
     }
+
     @DisplayName("roll() 테스트")
     @Test
     void canRoll() throws Exception {
-        Game game = new Game();
         game.roll(0);
+    }
+
+    @DisplayName("gutterGame")
+    @Test
+    void gutterGame() throws Exception {
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+            assertThat(game.getScore()).isEqualTo(0);
+        }
     }
 }
--- src/test/java/org/study/tddpasswordchecker/bowling/Game.java
+++ src/test/java/org/study/tddpasswordchecker/bowling/Game.java
@@ -1,6 +1,11 @@
 package org.study.tddpasswordchecker.bowling;
 
 public class Game {
+
     public void roll(int pins) {
+    }
+
+    public int getScore() {
+        return 0;
     }
 }
