a43c903078b0ce6f6974bf8ec2d3215ece9920ee
bizplus033 <bizplus033@naver.com>
2024-05-10 16:37:09 +0900

    [Fizz Buzz] 마무리 -> src/main/java 패키지로 FizzBuzz 클래스 이동

--- src/test/java/org/study/tddpasswordchecker/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/study/tddpasswordchecker/fizzbuzz/FizzBuzzTest.java
@@ -1,10 +1,9 @@
 package org.study.tddpasswordchecker.fizzbuzz;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
