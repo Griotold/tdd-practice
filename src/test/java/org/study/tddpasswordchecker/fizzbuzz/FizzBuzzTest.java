f517772c19fa6535ffd9ee372194318bb694f52c
bizplus033 <bizplus033@naver.com>
2024-05-10 15:33:52 +0900

    [Fizz Buzz] 리팩토링 - FizzBuzz 객체 필드로 추출

--- src/test/java/org/study/tddpasswordchecker/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/study/tddpasswordchecker/fizzbuzz/FizzBuzzTest.java
@@ -1,22 +1,28 @@
 package org.study.tddpasswordchecker.fizzbuzz;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void input_1_then_1_return() throws Exception {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.call(1);
         assertThat(result).isEqualTo("1");
     }
 
     @Test
     void input_3_then_Fizz_return() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.call(3);
         assertThat(result).isEqualTo("Fizz");
     }
