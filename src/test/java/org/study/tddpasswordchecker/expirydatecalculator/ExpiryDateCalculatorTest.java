3bc86b3d602c2fbee93ec39bff8414eedfb2dc16
Griotold <gotjd9773@naver.com>
2024-05-11 10:10:41 +0900

    [만료일 계산기] 4. 첫 납부일과 만료일의 일자가 같지 않을 때 1만원 납부하면 첫 납부일 기준으로 다음 만료일 정함 + 빌더 패턴 적용

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator/ExpiryDateCalculatorTest.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator/ExpiryDateCalculatorTest.java
@@ -24,10 +24,16 @@ public class ExpiryDateCalculatorTest {
     @DisplayName("만원 납부하면 한달 뒤가 만료일이 됨")
     void 만원_납부하면_한달_뒤가_만료일이_됨() throws Exception {
         assertExpiryDate(
-                new PayData(LocalDate.of(2019, 3, 1), 10_000),
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 4, 1));
         assertExpiryDate(
-                new PayData(LocalDate.of(2019, 5, 5), 10_000),
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 5, 5))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 6, 5));
 
     }
@@ -36,13 +42,33 @@ public class ExpiryDateCalculatorTest {
     @DisplayName("납부일과 한달 뒤 일자가 같지 않음")
     void 납부일과_한달_뒤_일자가_같지_않음() throws Exception {
         assertExpiryDate(
-                new PayData(LocalDate.of(2019, 1, 31), 10_000),
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 2, 28));
         assertExpiryDate(
-                new PayData(LocalDate.of(2019, 5, 31), 10_000),
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 5, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 6, 30));
         assertExpiryDate(
-                new PayData(LocalDate.of(2020, 1, 31), 10_000),
+                PayData.builder()
+                        .billingDate(LocalDate.of(2020,1,31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2020, 2, 29));
+    }
+
+    @Test
+    @DisplayName("첫 납부일과 만료일의 일자가 같지 않을 때 1만원 납부하면 첫 납부일 기준으로 다음 만료일 정함")
+    void 첫_납부일과_만료일_일자가_다를때_만원_납부() throws Exception {
+        PayData payData = new PayData(
+                LocalDate.of(2019, 1, 31),
+                LocalDate.of(2019, 2, 28),
+                10_000);
+
+        assertExpiryDate(payData, LocalDate.of(2019, 3, 31));
     }
 }
--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator/PayData.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator/PayData.java
@@ -3,12 +3,20 @@ package org.study.tddpasswordchecker.expirydatecalculator;
 import java.time.LocalDate;
 
 public class PayData {
+    private LocalDate firstBillingDate;
     private LocalDate billingDate;
     private int payAmount;
 
-    public PayData(LocalDate billingDate, int payAmount) {
+    public PayData() {}
+
+    public PayData(LocalDate firstBillingDate, LocalDate billingDate, int payAmount) {
+        this.firstBillingDate = firstBillingDate;
         this.billingDate = billingDate;
         this.payAmount = payAmount;
+    }
+
+    public LocalDate getFirstBillingDate() {
+        return firstBillingDate;
     }
 
     public LocalDate getBillingDate() {
@@ -17,5 +25,32 @@ public class PayData {
 
     public int getPayAmount() {
         return payAmount;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private PayData data = new PayData();
+
+        public Builder billingDate(LocalDate billingDate) {
+            data.billingDate = billingDate;
+            return this;
+        }
+
+        public Builder payAmount(int payAmount) {
+            data.payAmount = payAmount;
+            return this;
+        }
+
+        public Builder firstBillingDate(LocalDate firstBillingDate) {
+            data.firstBillingDate = firstBillingDate;
+            return this;
+        }
+
+        public PayData build() {
+            return data;
+        }
     }
 }
