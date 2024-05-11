1269ec993360e8ddc4f0411a03001d029984fd46
Griotold <gotjd9773@naver.com>
2024-05-11 09:36:32 +0900

    [만료일 계산기] 3. 납부일과 한달 뒤 일자가 같지 않는 경우 테스트 추가 -> 구현 코드에서 LocalData.plusMonths() 가 이미 예외상황을 처리해주고 있기 때문에 따로 건들지 않았음

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator/ExpiryDateCalculatorTest.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator/ExpiryDateCalculatorTest.java
@@ -23,11 +23,26 @@ public class ExpiryDateCalculatorTest {
     @Test
     @DisplayName("만원 납부하면 한달 뒤가 만료일이 됨")
     void 만원_납부하면_한달_뒤가_만료일이_됨() throws Exception {
-        assertExpiryDate(LocalDate.of(2019, 3, 1), 10_000,
+        assertExpiryDate(
+                LocalDate.of(2019, 3, 1), 10_000,
                 LocalDate.of(2019, 4, 1));
-
-        assertExpiryDate(LocalDate.of(2019, 5, 5), 10_000,
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 5), 10_000,
                 LocalDate.of(2019, 6, 5));
 
+    }
+
+    @Test
+    @DisplayName("납부일과 한달 뒤 일자가 같지 않음")
+    void 납부일과_한달_뒤_일자가_같지_않음() throws Exception {
+        assertExpiryDate(
+                LocalDate.of(2019, 1, 31), 10_000,
+                LocalDate.of(2019, 2, 28));
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 31), 10_000,
+                LocalDate.of(2019, 6, 30));
+        assertExpiryDate(
+                LocalDate.of(2020, 1, 31), 10_000,
+                LocalDate.of(2020, 2, 29));
     }
 }
