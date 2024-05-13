a0583754a784e2ed8cde5e9c5e23b141912438ba
bizplus033 <bizplus033@naver.com>
2024-05-13 16:57:58 +0900

    [FizzBuzzBot ] 2. 3을 넣었을 때 "Fizz"

--- src/test/java/org/study/tddpasswordchecker/fizzbuzz0513/FizzBuzzBot.java
+++ src/test/java/org/study/tddpasswordchecker/fizzbuzz0513/FizzBuzzBot.java
@@ -2,6 +2,7 @@ package org.study.tddpasswordchecker.fizzbuzz0513;
 
 public class FizzBuzzBot {
     public String fizzBuzz(int i) {
+        if (i == 3) return "Fizz";
         return String.valueOf(i);
     }
 }
--- src/test/java/org/study/tddpasswordchecker/fizzbuzz0513/FizzBuzzTest.java
+++ src/test/java/org/study/tddpasswordchecker/fizzbuzz0513/FizzBuzzTest.java
@@ -1,8 +1,9 @@
 package org.study.tddpasswordchecker.fizzbuzz0513;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class FizzBuzzTest {
 
@@ -10,6 +11,13 @@ public class FizzBuzzTest {
     void input_1_then_return_1() throws Exception {
         FizzBuzzBot fizzBuzzBot = new FizzBuzzBot();
         String result = fizzBuzzBot.fizzBuzz(1);
-        Assertions.assertThat(result).isEqualTo("1");
+        assertThat(result).isEqualTo("1");
+    }
+
+    @Test
+    void input_3_then_return_Fizz() throws Exception {
+        FizzBuzzBot fizzBuzzBot = new FizzBuzzBot();
+        String result = fizzBuzzBot.fizzBuzz(3);
+        assertThat(result).isEqualTo("Fizz");
     }
 }
