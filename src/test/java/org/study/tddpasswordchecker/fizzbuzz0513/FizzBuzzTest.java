e800d157764a3d1dfe1f4fd1bdc5e259ce763346
bizplus033 <bizplus033@naver.com>
2024-05-13 16:58:53 +0900

    [FizzBuzzBot ] 리팩토링 - FizzBuzzBot 객체 필드로 추출

--- src/test/java/org/study/tddpasswordchecker/fizzbuzz0513/FizzBuzzTest.java
+++ src/test/java/org/study/tddpasswordchecker/fizzbuzz0513/FizzBuzzTest.java
@@ -1,22 +1,28 @@
 package org.study.tddpasswordchecker.fizzbuzz0513;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class FizzBuzzTest {
 
+    FizzBuzzBot fizzBuzzBot;
+
+    @BeforeEach
+    public void setUp() {
+        fizzBuzzBot = new FizzBuzzBot();
+    }
+
     @Test
     void input_1_then_return_1() throws Exception {
-        FizzBuzzBot fizzBuzzBot = new FizzBuzzBot();
         String result = fizzBuzzBot.fizzBuzz(1);
         assertThat(result).isEqualTo("1");
     }
 
     @Test
     void input_3_then_return_Fizz() throws Exception {
-        FizzBuzzBot fizzBuzzBot = new FizzBuzzBot();
         String result = fizzBuzzBot.fizzBuzz(3);
         assertThat(result).isEqualTo("Fizz");
     }
