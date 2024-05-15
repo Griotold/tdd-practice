6fba8136eef1af08af1fcfb59c357c57ca96098b
Griotold <gotjd9773@naver.com>
2024-05-15 11:16:32 +0900

    [만료일 계산기 - 0515] 예를 추가하면서 구현을 일반화

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515.java
@@ -4,6 +4,6 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator0515 {
     public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return LocalDate.of(2019, 4, 1);
+        return billingDate.plusMonths(1);
     }
 }
--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.*;
+
 /**
  * 240515 - 3장 요약을 위해 다시 구현
  * */
@@ -17,7 +19,14 @@ public class ExpiryDateCalculator0515Test {
 
         ExpiryDateCalculator0515 cal = new ExpiryDateCalculator0515();
         LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-        Assertions.assertThat(expiryDate).isEqualTo(LocalDate.of(2019, 4, 1));
+        assertThat(expiryDate).isEqualTo(LocalDate.of(2019, 4, 1));
+
+        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
+        int payAmount2 = 10_000;
+
+        ExpiryDateCalculator0515 cal2 = new ExpiryDateCalculator0515();
+        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
+        assertThat(expiryDate2).isEqualTo(LocalDate.of(2019, 6, 5));
 
     }
 
