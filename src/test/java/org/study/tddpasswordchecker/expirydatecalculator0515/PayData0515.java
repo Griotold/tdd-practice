111b56128c1fd4680694c75a24140048eb3f406e
Griotold <gotjd9773@naver.com>
2024-05-15 12:10:50 +0900

    [만료일 계산기 - 0515] PayData0515 에 빌더 패턴 추가

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/PayData0515.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/PayData0515.java
@@ -3,14 +3,20 @@ package org.study.tddpasswordchecker.expirydatecalculator0515;
 import java.time.LocalDate;
 
 public class PayData0515 {
+    private LocalDate firstBillingDate;
     private LocalDate billingDate;
     private int payAmount;
 
     private PayData0515() {}
 
-    public PayData0515(LocalDate billingDate, int payAmount) {
+    public PayData0515(LocalDate firstBillingDate, LocalDate billingDate, int payAmount) {
+        this.firstBillingDate = firstBillingDate;
         this.billingDate = billingDate;
         this.payAmount = payAmount;
+    }
+
+    public LocalDate getFirstBillingDate() {
+        return firstBillingDate;
     }
 
     public LocalDate getBillingDate() {
@@ -19,6 +25,33 @@ public class PayData0515 {
 
     public int getPayAmount() {
         return payAmount;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private PayData0515 data = new PayData0515();
+
+        public Builder firstBillingDate(LocalDate firstBillingDate) {
+            data.firstBillingDate = firstBillingDate;
+            return this;
+        }
+
+        public Builder billingDate(LocalDate billingDate) {
+            data.billingDate = billingDate;
+            return this;
+        }
+
+        public Builder payAmount(int payAmount) {
+            data.payAmount = payAmount;
+            return this;
+        }
+
+        public PayData0515 build() {
+            return data;
+        }
     }
 
 }
