676453f6dbe0fd72db9fdf054ecd44c9214202e0
Griotold <gotjd9773@naver.com>
2024-05-15 12:13:51 +0900

    [만료일 계산기 - 0515] 테스트 코드들 빌더 패턴 적용

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
@@ -20,24 +20,49 @@ public class ExpiryDateCalculator0515Test {
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() throws Exception {
         assertExpiryDate(
-                new PayData0515(LocalDate.of(2019, 3, 1), 10_000),
+                PayData0515.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 4, 1));
         assertExpiryDate(
-                new PayData0515(LocalDate.of(2019, 5, 5), 10_000),
+                PayData0515.builder()
+                        .billingDate(LocalDate.of(2019, 5, 5))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 6, 5));
     }
 
     @Test
     void 납부일과_한달_뒤_일자가_같지_않음() throws Exception {
         assertExpiryDate(
-                new PayData0515(LocalDate.of(2019, 1, 31), 10_000),
+                PayData0515.builder()
+                        .billingDate(LocalDate.of(2019, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 2, 28));
         assertExpiryDate(
-                new PayData0515(LocalDate.of(2019, 5, 31), 10_000),
+                PayData0515.builder()
+                        .billingDate(LocalDate.of(2019, 5, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 6, 30));
         assertExpiryDate(
-                new PayData0515(LocalDate.of(2020, 1, 31), 10_000),
+                PayData0515.builder()
+                        .billingDate(LocalDate.of(2020, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2020, 2, 29));
+    }
+
+    @Test
+    void 첫_납부일과_만료일_일자가_다를때_만원_납부() {
+        assertExpiryDate(
+                PayData0515.builder()
+                        .billingDate(LocalDate.of(2020, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
+                LocalDate.of(2019, 3, 31));
     }
 
 }
