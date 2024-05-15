b475359438dec9fe58934005786325acb7083830
Griotold <gotjd9773@naver.com>
2024-05-15 15:53:23 +0900

    [만료일 계산기 - 0515] 5. 첫 납부일과 만료일 일자가 다를 때 이만원 이상 납부

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515.java
@@ -1,6 +1,7 @@
 package org.study.tddpasswordchecker.expirydatecalculator0515;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator0515 {
 
@@ -9,6 +10,9 @@ public class ExpiryDateCalculator0515 {
         if (payData.getFirstBillingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                if (YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+                }
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
