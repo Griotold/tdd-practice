2251246ff15eadcad97daf3e079f711b0ba0270b
Griotold <gotjd9773@naver.com>
2024-05-15 11:13:55 +0900

    [만료일 계산기 - 0515] 1. 만원 납부하면 한달 뒤가 만료일이 됨

---
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515.java
@@ -0,0 +1,9 @@
+package org.study.tddpasswordchecker.expirydatecalculator0515;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator0515 {
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019, 4, 1);
+    }
+}
--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
@@ -1,6 +1,9 @@
 package org.study.tddpasswordchecker.expirydatecalculator0515;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 /**
  * 240515 - 3장 요약을 위해 다시 구현
@@ -8,12 +11,14 @@ import org.junit.jupiter.api.Test;
 public class ExpiryDateCalculator0515Test {
 
     @Test
-    void name() throws Exception {
-        // given
+    void 만원_납부하면_한달_뒤가_만료일이_됨() throws Exception {
+        LocalDate billingDate = LocalDate.of(2019, 3, 1);
+        int payAmount = 10_000;
 
-        // when
+        ExpiryDateCalculator0515 cal = new ExpiryDateCalculator0515();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        Assertions.assertThat(expiryDate).isEqualTo(LocalDate.of(2019, 4, 1));
 
-        // then
     }
 
 }
