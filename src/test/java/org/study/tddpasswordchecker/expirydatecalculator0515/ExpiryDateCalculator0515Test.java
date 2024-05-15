84e12758429c14c73514a1844ba9e172e013152b
Griotold <gotjd9773@naver.com>
2024-05-15 11:22:55 +0900

    [만료일 계산기 - 0515] 리팩토링 - 코드 정리: 테스트 코드 중복 제거

--- src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
+++ src/test/java/org/study/tddpasswordchecker/expirydatecalculator0515/ExpiryDateCalculator0515Test.java
@@ -1,33 +1,32 @@
 package org.study.tddpasswordchecker.expirydatecalculator0515;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 240515 - 3장 요약을 위해 다시 구현
- * */
+ */
 public class ExpiryDateCalculator0515Test {
+
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate) {
+        ExpiryDateCalculator0515 cal = new ExpiryDateCalculator0515();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+        assertThat(expiryDate).isEqualTo(expectedExpiryDate);
+    }
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨() throws Exception {
-        LocalDate billingDate = LocalDate.of(2019, 3, 1);
-        int payAmount = 10_000;
 
-        ExpiryDateCalculator0515 cal = new ExpiryDateCalculator0515();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-        assertThat(expiryDate).isEqualTo(LocalDate.of(2019, 4, 1));
+        assertExpiryDate(
+                LocalDate.of(2019, 3, 1), 10_000,
+                LocalDate.of(2019, 4, 1));
 
-        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-        int payAmount2 = 10_000;
-
-        ExpiryDateCalculator0515 cal2 = new ExpiryDateCalculator0515();
-        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-        assertThat(expiryDate2).isEqualTo(LocalDate.of(2019, 6, 5));
+        assertExpiryDate(
+                LocalDate.of(2019, 5, 5), 10_000,
+                LocalDate.of(2019, 6, 5));
 
     }
-
 }
