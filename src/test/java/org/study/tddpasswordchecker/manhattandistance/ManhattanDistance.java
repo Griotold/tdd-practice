f34beacbeb3d00755a7a06453e5f41dc6cdf4859
bizplus033 <bizplus033@naver.com>
2024-05-16 16:51:14 +0900

    [Manhattan Distance] 1. from [1 , 1] to [1, 1] -> 0

---
+++ src/test/java/org/study/tddpasswordchecker/manhattandistance/ManhattanDistance.java
@@ -0,0 +1,7 @@
+package org.study.tddpasswordchecker.manhattandistance;
+
+public class ManhattanDistance {
+    public int calculate(Point point1, Point point2) {
+        return 0;
+    }
+}
--- src/test/java/org/study/tddpasswordchecker/manhattandistance/ManhattanDistanceTest.java
+++ src/test/java/org/study/tddpasswordchecker/manhattandistance/ManhattanDistanceTest.java
@@ -2,10 +2,14 @@ package org.study.tddpasswordchecker.manhattandistance;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ManhattanDistanceTest {
 
     @Test
-    void name() {
-
+    void from_one_and_one_to_one_and_one_then_return_0() {
+        ManhattanDistance manhattanDistance = new ManhattanDistance();
+        int result = manhattanDistance.calculate(new Point(1, 1), new Point(1, 1));
+        assertThat(result).isEqualTo(0);
     }
 }
