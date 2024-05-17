f665bbe2b654b5c0541c3b56e8f41a2e0f20540a
bizplus033 <bizplus033@naver.com>
2024-05-17 16:08:50 +0900

    [Prime Factors] 리팩토링 - primeFactors 객체 필드로 추출

--- src/test/java/org/study/tddpasswordchecker/primefactor/PrimeFactorsTest.java
+++ src/test/java/org/study/tddpasswordchecker/primefactor/PrimeFactorsTest.java
@@ -1,23 +1,30 @@
 package org.study.tddpasswordchecker.primefactor;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactorsTest {
+
+    PrimeFactors primeFactors;
+
+    @BeforeEach
+    void setUp() {
+        primeFactors = new PrimeFactors();
+    }
     @Test
     void one_then_return_empty_list() {
-        PrimeFactors primeFactors = new PrimeFactors();
         List<Integer> result = primeFactors.generate(1);
         Assertions.assertThat(result).isEqualTo(Collections.emptyList());
     }
 
     @Test
     void two_then_return_list_with_two() {
-        PrimeFactors primeFactors = new PrimeFactors();
         List<Integer> result = primeFactors.generate(2);
         Assertions.assertThat(result).isEqualTo(List.of(2));
     }
+
 }
