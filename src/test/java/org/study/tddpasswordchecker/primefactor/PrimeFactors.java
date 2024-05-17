3bd6aa406279c5f947cc7e1d582c576844cec6f8
bizplus033 <bizplus033@naver.com>
2024-05-17 15:15:04 +0900

    [Prime Factors] 1. 1을 넣으면 Empty List

---
+++ src/test/java/org/study/tddpasswordchecker/primefactor/PrimeFactors.java
@@ -0,0 +1,10 @@
+package org.study.tddpasswordchecker.primefactor;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PrimeFactors {
+    public List<Integer> generate(int input) {
+        return Collections.emptyList();
+    }
+}
--- src/test/java/org/study/tddpasswordchecker/primefactor/PrimeFactorsTest.java
+++ src/test/java/org/study/tddpasswordchecker/primefactor/PrimeFactorsTest.java
@@ -1,10 +1,16 @@
 package org.study.tddpasswordchecker.primefactor;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
 
 public class PrimeFactorsTest {
     @Test
-    void name() {
-
+    void one_then_return_empty_list() {
+        PrimeFactors primeFactors = new PrimeFactors();
+        List<Integer> result = primeFactors.generate(1);
+        Assertions.assertThat(result).isEqualTo(Collections.emptyList());
     }
 }
