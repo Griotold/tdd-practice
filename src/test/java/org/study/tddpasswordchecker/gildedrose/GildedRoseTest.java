46f743374851b717ad052948d0897fd10eb58c84
Griotold <gotjd9773@naver.com>
2024-05-18 12:10:33 +0900

    [Gilded Roes] 테스트를 통해 GildedRose#updateQuality() 동작 방식 알아보기 - 'Backstage passes'

--- src/test/java/org/study/tddpasswordchecker/gildedrose/GildedRoseTest.java
+++ src/test/java/org/study/tddpasswordchecker/gildedrose/GildedRoseTest.java
@@ -48,7 +48,44 @@ public class GildedRoseTest {
             assertThat(itemQuality()).isEqualTo(21);
             assertThat(itemSellIn()).isEqualTo(9);
         }
-
     }
 
+    @Nested
+    @DisplayName("'Backstage passes'는")
+    class BackstagePasses {
+        @Test
+        @DisplayName("10일 초과 남았을 때 하루가 지나면 품질이 1 씩 증가하고 sellIn은 감소한다")
+        void quality1IncreasesSellInDecreasesMoreThan10Days() {
+            GildedRoseTest.this.setup("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+            app.updateQuality();
+            assertThat(itemQuality()).isEqualTo(21);
+            assertThat(itemSellIn()).isEqualTo(14);
+        }
+
+        @Test
+        @DisplayName("6일에서 10일 남았을 때 하루가 지나면 품질이 2 씩 증가하고 sellIn은 감소한다")
+        void quality2IncreasesSellInDecreases6to10Days() {
+            GildedRoseTest.this.setup("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+            app.updateQuality();
+            assertThat(itemQuality()).isEqualTo(22);
+            assertThat(itemSellIn()).isEqualTo(9);
+        }
+
+        @Test
+        @DisplayName("5일 미만 남았을 때 하루가 지나면 품질이 3 씩 증가하고 sellIn은 감소한다")
+        void quality3IncreasesSellInDecreasesLessThan6Days() {
+            GildedRoseTest.this.setup("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+            app.updateQuality();
+            assertThat(itemQuality()).isEqualTo(23);
+            assertThat(itemSellIn()).isEqualTo(4);
+        }
+
+        @Test
+        @DisplayName("콘서트가 끝나면(즉, sellIn이 0 이하가 되면) 품질은 0이된다.")
+        void qualityDropsToZeroAfterTheConcert() {
+            GildedRoseTest.this.setup("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+            app.updateQuality();
+            assertThat(itemQuality()).isEqualTo(0);
+        }
+    }
 }
