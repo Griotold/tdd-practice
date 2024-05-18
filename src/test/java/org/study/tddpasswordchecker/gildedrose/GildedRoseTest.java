59f63343d8bf87d93fca719520f3e9b9894298a2
Griotold <gotjd9773@naver.com>
2024-05-18 12:05:35 +0900

    [Gilded Roes] 테스트를 통해 GildedRose#updateQuality() 동작 방식 알아보기 - 일반 아이템 - StandardItem

--- src/test/java/org/study/tddpasswordchecker/gildedrose/GildedRoseTest.java
+++ src/test/java/org/study/tddpasswordchecker/gildedrose/GildedRoseTest.java
@@ -1,16 +1,40 @@
 package org.study.tddpasswordchecker.gildedrose;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class GildedRoseTest {
+import static org.assertj.core.api.Assertions.*;
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        Assertions.assertThat("fixme").isEqualTo(app.items[0].name);
+public class GildedRoseTest {
+    private Item[] items;
+    private GildedRose app;
+
+    private void setup(String name, int sellIn, int quality) {
+        items = new Item[] {new Item(name, sellIn, quality)};
+        app = new GildedRose(items);
+    }
+
+    private int itemSellIn() {
+        return app.items[0].sellIn;
+    }
+
+    private int itemQuality() {
+        return app.items[0].quality;
+    }
+
+    @Nested
+    @DisplayName("일반 아이템은")
+    class StandardItem {
+        @DisplayName("하루가 지나면 Quality 와 sellIn 이 1씩 감소한다")
+        @Test
+        void qualityAndSellInDecrease() throws Exception {
+            GildedRoseTest.this.setup("Standard Item", 10, 20);
+            app.updateQuality();
+            assertThat(itemQuality()).isEqualTo(19);
+            assertThat(itemSellIn()).isEqualTo(9);
+        }
     }
 
 }
