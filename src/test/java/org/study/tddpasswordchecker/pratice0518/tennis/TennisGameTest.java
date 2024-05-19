48e5c01e73e65f09bab1cb3c67b0a616f40466ab
Griotold <gotjd9773@naver.com>
2024-05-19 11:31:51 +0900

    [Tennis Game] 리팩토링 - TennisGame 객체 필드 추출

--- src/test/java/org/study/tddpasswordchecker/pratice0518/tennis/TennisGameTest.java
+++ src/test/java/org/study/tddpasswordchecker/pratice0518/tennis/TennisGameTest.java
@@ -1,21 +1,26 @@
 package org.study.tddpasswordchecker.pratice0518.tennis;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisGameTest {
+    TennisGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new TennisGame("Iga Swiatek", "Aryna Sabalenka");
+    }
 
     @Test
     void testNewGameShouldReturnLoveAll() throws Exception {
-        TennisGame game = new TennisGame("Iga Swiatek", "Aryna Sabalenka");
         String score = game.getScore();
         assertThat(score).isEqualTo("Love all");
     }
 
     @Test
     void testPlayerOneWinsFirstBall() throws Exception {
-        TennisGame game = new TennisGame("Iga Swiatek", "Aryna Sabalenka");
         game.playerOneScores();
         String score = game.getScore();
         assertThat(score).isEqualTo("Fifteen, Love");
@@ -23,7 +28,6 @@ public class TennisGameTest {
 
     @Test
     void testFifteenAll() throws Exception {
-        TennisGame game = new TennisGame("Iga Swiatek", "Aryna Sabalenka");
         game.playerOneScores();
         game.playerTwoScores();
         String score = game.getScore();
