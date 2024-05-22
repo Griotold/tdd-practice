ea8549675b2640ac588ebc8bd14647dde7b8291e
bizplus033 <bizplus033@naver.com>
2024-05-23 08:27:19 +0900

    [Tennis] 리팩토링 - Tennis 객체 필드 추출, 선수 이름 받는 생성자

--- src/test/java/org/study/tddpasswordchecker/pratice0518/tennisGame/Tennis.java
+++ src/test/java/org/study/tddpasswordchecker/pratice0518/tennisGame/Tennis.java
@@ -3,6 +3,14 @@ package org.study.tddpasswordchecker.pratice0518.tennisGame;
 public class Tennis {
     private int playerOneScore;
     private int playerTwoScore;
+    private String playerOneName;
+    private String playerTwoName;
+
+    public Tennis(String playerOneName, String playerTwoName) {
+        this.playerOneName = playerOneName;
+        this.playerTwoName = playerTwoName;
+    }
+
     public String getScore() {
         if (playerOneScore == 1) return "Fifteen,Love";
         return "Love all";
--- src/test/java/org/study/tddpasswordchecker/pratice0518/tennisGame/TennisTest.java
+++ src/test/java/org/study/tddpasswordchecker/pratice0518/tennisGame/TennisTest.java
@@ -1,21 +1,27 @@
 package org.study.tddpasswordchecker.pratice0518.tennisGame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class TennisTest {
 
+    Tennis tennis;
+
+    @BeforeEach
+    void setUp() {
+        tennis = new Tennis("Novak Dokovic", "Rafael Nadal");
+    }
+
     @Test
     void testNewGameShouldReturnLoveAll() {
-        Tennis tennis = new Tennis();
         String score = tennis.getScore();
         assertThat(score).isEqualTo("Love all");
     }
 
     @Test
     void testPlayerOneWinsFirstBall() {
-        Tennis tennis = new Tennis();
         tennis.playerOneScores();
         String score = tennis.getScore();
         assertThat(score).isEqualTo("Fifteen,Love");
