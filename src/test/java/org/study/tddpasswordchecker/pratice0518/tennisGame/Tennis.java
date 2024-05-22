aae74df72b91bfa3a31aac3d81295a27dceba2e1
bizplus033 <bizplus033@naver.com>
2024-05-23 08:20:28 +0900

    [Tennis] 1. 테니스 객체를 생성하고 getScore() 를 호출하면 "Love all"

---
+++ src/test/java/org/study/tddpasswordchecker/pratice0518/tennisGame/Tennis.java
@@ -0,0 +1,7 @@
+package org.study.tddpasswordchecker.pratice0518.tennisGame;
+
+public class Tennis {
+    public String getScore() {
+        return "Love all";
+    }
+}
---
+++ src/test/java/org/study/tddpasswordchecker/pratice0518/tennisGame/TennisTest.java
@@ -0,0 +1,15 @@
+package org.study.tddpasswordchecker.pratice0518.tennisGame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TennisTest {
+
+    @Test
+    void testNewGameShouldReturnLoveAll() {
+        Tennis tennis = new Tennis();
+        String score = tennis.getScore();
+        assertThat(score).isEqualTo("Love all");
+    }
+}
