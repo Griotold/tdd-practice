9a1a481009d76ace4f0f7f520230f66d6ffbb50d
bizplus033 <bizplus033@naver.com>
2024-05-31 15:21:27 +0900

    [BowlingGame - 0531] 3번 부터 다시 시작

--- src/test/java/org/study/tddpasswordchecker/practice0531/BowlingGame.java
+++ src/test/java/org/study/tddpasswordchecker/practice0531/BowlingGame.java
@@ -1,22 +1,13 @@
 package org.study.tddpasswordchecker.practice0531;
 
 public class BowlingGame {
-    private Frame[] frames = new Frame[10];
     private int score;
-    private int index;
 
     public BowlingGame() {
-        for (int i = 0; i < 10; i++) {
-            frames[i] = new Frame();
-        }
-        score = 0;
-        index = 0;
+
     }
     public void roll(int pins) {
-        if (frames[index].isFrameFinished()) {
-            index++;
-        }
-        score += frames[index].roll(pins);
+       score += pins;
     }
 
     public int getScore() {
--- src/test/java/org/study/tddpasswordchecker/practice0531/BowlingGameTest.java
+++ src/test/java/org/study/tddpasswordchecker/practice0531/BowlingGameTest.java
@@ -1,11 +1,9 @@
 package org.study.tddpasswordchecker.practice0531;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BowlingGameTest {
     BowlingGame bowlingGame;
@@ -15,37 +13,29 @@ public class BowlingGameTest {
         bowlingGame = new BowlingGame();
     }
 
+    private void rollMany(int times, int pins) {
+        for (int i = 0; i < times; i++) {
+            bowlingGame.roll(pins);
+        }
+    }
+
     @Test
     void canRoll() {
         bowlingGame.roll(0);
     }
-
     @Test
     void gutterGame() {
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(0);
-        }
+        rollMany(20, 0);
         int score = bowlingGame.getScore();
         assertThat(score).isEqualTo(0);
     }
 
     @Test
-    void allRollsKnockDownOnePin() {
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(1);
-        }
+    void testAllOnes() {
+        rollMany(20, 1);
         int score = bowlingGame.getScore();
         assertThat(score).isEqualTo(20);
     }
-    @Disabled
-    @Test
-    void strikeFirstFrame() {
-        bowlingGame.roll(10); // first frame end!
-        bowlingGame.roll(6);
-        bowlingGame.roll(3);
-        rollMany(16, 0);
-        int score = bowlingGame.getScore();
-        assertThat(score).isEqualTo(27);
-    }
+
 
 }
