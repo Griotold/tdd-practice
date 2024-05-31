98bfbf833dc75920991fd070f54d4fb39e110ec5
bizplus033 <bizplus033@naver.com>
2024-05-31 15:35:06 +0900

    [BowlingGame - 0531] 스페어를 테스트하려고 했는데 문제점 발견! roll() 메서드는 score 에 관해서 연산하고 있지만 메소드명이 그것을 암시하지 못하고 있음. 반면에, getScore() 메서드는 score 에 관해서 연산을 안하고 있지만 메서드명이 연산을 하고 있는것처럼 암시함. 이것은 잘못된 설계

--- src/test/java/org/study/tddpasswordchecker/practice0531/BowlingGameTest.java
+++ src/test/java/org/study/tddpasswordchecker/practice0531/BowlingGameTest.java
@@ -1,6 +1,7 @@
 package org.study.tddpasswordchecker.practice0531;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,6 +36,16 @@ public class BowlingGameTest {
         rollMany(20, 1);
         int score = bowlingGame.getScore();
         assertThat(score).isEqualTo(20);
+    }
+    @Disabled
+    @Test
+    void testOneSpare() {
+        bowlingGame.roll(5);
+        bowlingGame.roll(5);
+        bowlingGame.roll(3);
+        rollMany(17, 0);
+        int score = bowlingGame.getScore();
+        assertThat(score).isEqualTo(16);
     }
 
 
