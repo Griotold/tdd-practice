1739acadd8f477edb8686da1091cfc02620b4e8d
bizplus033 <bizplus033@naver.com>
2024-05-31 16:05:51 +0900

    [Queue] 나만의 큐 구현하기 시작

---
+++ src/test/java/org/study/tddpasswordchecker/practice0531/queue/QueueTest.java
@@ -0,0 +1,14 @@
+package org.study.tddpasswordchecker.practice0531.queue;
+
+import org.junit.jupiter.api.Test;
+
+// The queue should have the following methods:
+//Enqueue(object): Adds an element to the queue.
+//Dequeue(): Returns the first element and removes it from the queue.
+//Peek(): Returns the first element without removing it.
+public class QueueTest {
+    @Test
+    void name() {
+
+    }
+}
