3b08c03378fbd47365e44a2bc5013f08f62d1ad2
Griotold <gotjd9773@naver.com>
2024-06-07 11:34:14 +0900

    [코딩테스트 문제 - tdd 로 풀기 - 1.String] 1-3. 문장 속 단어 - 1. 공백 문자열을 넣으면 IllegalArgumentException 발생

---
+++ src/test/java/org/study/tddpasswordchecker/kim/string/LongWordInString.java
@@ -0,0 +1,9 @@
+package org.study.tddpasswordchecker.kim.string;
+
+public class LongWordInString {
+
+    public String getLongWord(String input) {
+        if (input.isEmpty()) throw new IllegalArgumentException();
+        return null;
+    }
+}
---
+++ src/test/java/org/study/tddpasswordchecker/kim/string/LongWordInStringTest.java
@@ -0,0 +1,37 @@
+package org.study.tddpasswordchecker.kim.string;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class LongWordInStringTest {
+
+    private LongWordInString longWordInString;
+
+    @BeforeEach
+    void setUp() {
+        this.longWordInString = new LongWordInString();
+    }
+
+    @DisplayName("공백 문자열을 넣으면 IllegalArgumentException")
+    @Test
+    void testEmptyString() {
+        assertThatThrownBy(() -> longWordInString.getLongWord(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+    @Disabled
+    @DisplayName("it is time to study -> study")
+    @Test
+    void testOne() {
+        String input = "it is time to study";
+        String reuslt = longWordInString.getLongWord(input);
+        assertThat(reuslt).isEqualTo("study");
+    }
+}
