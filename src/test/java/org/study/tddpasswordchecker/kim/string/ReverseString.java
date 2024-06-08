158fa6ddcbfe73c48577bc8671bfd93df30e2df8
Griotold <gotjd9773@naver.com>
2024-06-08 10:44:51 +0900

    [코딩테스트 문제 - tdd 로 풀기 - 1.String] 1-4. 단어 뒤집기 - (2). "" 이 입력되면 그대로 "" 리턴

--- src/test/java/org/study/tddpasswordchecker/kim/string/ReverseString.java
+++ src/test/java/org/study/tddpasswordchecker/kim/string/ReverseString.java
@@ -3,6 +3,6 @@ package org.study.tddpasswordchecker.kim.string;
 public class ReverseString {
     public String reverse(String input) {
         if (input == null) throw new IllegalArgumentException();
-        return null;
+        return "";
     }
 }
--- src/test/java/org/study/tddpasswordchecker/kim/string/ReverseStringTest.java
+++ src/test/java/org/study/tddpasswordchecker/kim/string/ReverseStringTest.java
@@ -1,9 +1,11 @@
 package org.study.tddpasswordchecker.kim.string;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ReverseStringTest {
 
@@ -17,8 +19,15 @@ public class ReverseStringTest {
     @DisplayName("null 이 입력되면 예외")
     @Test
     void testOne() {
-        Assertions.assertThatThrownBy(() -> reverseString.reverse(null))
+        assertThatThrownBy(() -> reverseString.reverse(null))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("공백문자는 그대로 공백이 리턴")
+    @Test
+    void testTwo() {
+        String result = reverseString.reverse("");
+        assertThat(result).isEqualTo("");
     }
 
 }
