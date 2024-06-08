ead86a8bf15e100340ee668edab1bdcee96634b6
Griotold <gotjd9773@naver.com>
2024-06-08 10:48:22 +0900

    [코딩테스트 문제 - tdd 로 풀기 - 1.String] 1-4. 단어 뒤집기 - 리팩터링 - 변수 인라인화 -> 이게 가독성이 높아지는 건지 잘모르겠음ㅋㅋ

--- src/test/java/org/study/tddpasswordchecker/kim/string/ReverseString.java
+++ src/test/java/org/study/tddpasswordchecker/kim/string/ReverseString.java
@@ -3,8 +3,6 @@ package org.study.tddpasswordchecker.kim.string;
 public class ReverseString {
     public String reverse(String input) {
         if (input == null) throw new IllegalArgumentException();
-        StringBuilder sb = new StringBuilder(input);
-        StringBuilder reverse = sb.reverse();
-        return reverse.toString();
+        return new StringBuilder(input).reverse().toString();
     }
 }
