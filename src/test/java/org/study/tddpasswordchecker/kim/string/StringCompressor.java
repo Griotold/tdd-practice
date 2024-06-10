9784680d2acd4cb31e0f1c109e5b5f9daccdabea
Griotold <gotjd9773@naver.com>
2024-06-10 15:59:20 +0900

    [코딩테스트 문제 - tdd 로 풀기 - 1.String] 1 - 11. 문자열 압축 - 1. "abccc" -> "abc3"

---
+++ src/test/java/org/study/tddpasswordchecker/kim/string/StringCompressor.java
@@ -0,0 +1,7 @@
+package org.study.tddpasswordchecker.kim.string;
+
+public class StringCompressor {
+    public String compress(String input) {
+        return "abc3";
+    }
+}
---
+++ src/test/java/org/study/tddpasswordchecker/kim/string/StringCompressorTest.java
@@ -0,0 +1,26 @@
+package org.study.tddpasswordchecker.kim.string;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCompressorTest {
+    private StringCompressor stringCompressor;
+
+    @BeforeEach
+    void setUp() {
+        this.stringCompressor = new StringCompressor();
+    }
+    private void assertCompress(String input, String expected) {
+        String result = stringCompressor.compress(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("abccc -> abc3")
+    @Test
+    void testOne() {
+        assertCompress("abccc", "abc3");
+    }
+}
