0717ec7286873ae3945465d12a6dd487f359797e
Griotold <gotjd9773@naver.com>
2024-06-10 11:47:14 +0900

    [코딩테스트 문제 - tdd 로 풀기 - 1.String] 1-8. 숫자만 추출 - 2. 문제에서 제시한 테스트 케이스

--- src/test/java/org/study/tddpasswordchecker/kim/string/ExtractNumberTest.java
+++ src/test/java/org/study/tddpasswordchecker/kim/string/ExtractNumberTest.java
@@ -1,6 +1,7 @@
 package org.study.tddpasswordchecker.kim.string;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,9 +15,24 @@ public class ExtractNumberTest {
         this.extractNumber = new ExtractNumber();
     }
 
+    @DisplayName("a1b2 -> 12")
     @Test
     void testOne() {
         int result = extractNumber.extract("a1b2");
         assertThat(result).isEqualTo(12);
+    }
+
+    @DisplayName("tge0a1h205er -> 1205")
+    @Test
+    void testTwo() {
+        int result = extractNumber.extract("tge0a1h205er");
+        assertThat(result).isEqualTo(1205);
+    }
+
+    @DisplayName("g0en2T0s8eSoft -> 208")
+    @Test
+    void testThree() {
+        int result = extractNumber.extract("g0en2T0s8eSoft");
+        assertThat(result).isEqualTo(208);
     }
 }
